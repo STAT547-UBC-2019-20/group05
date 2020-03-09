681b814a7dc2fdba03cd946e36070f6b36414346
Jacob Gerlofs <jgerlofs@mail.ubc.ca>
2020-03-09 00:57:37 -0700

    updates to README, some troubleshooting with data cleaning functions within EDA script

--- scripts/EDA.R
+++ scripts/EDA.R
@@ -1,26 +1,46 @@
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
+library(stringr)
+library(purrr)
 
 #Load in survey_data csv file
 survey_data <- read.csv(here::here("data", "survey_data.csv"))
 view(survey_data)
 
-fix_likert_1 <- function (df){
+fix_likert_1 <- function(df){
   for (i in seq_along(df)){
-    if (i == '7 = Extremely satisfied'){
-      df[[i]] <- 7
-    }
-    else if (i == '4 = Neither satisfied nor dissatisfied'){
-      df[[i]] <- 4
-    }
-    else if (i == '1 = Not at all satisfied'){
-      df[[i]] <- 1
-    }
+    str_replace(df, "7 = Extremely satisfied", "7")
+    str_replace(df, "4 = Neither satisfied nor dissatisfied", "4")
+    str_replace(df, "1 = Not at all satisfied", "1")
   }
 }
 
-map(survey_data, function(df) fix_likert_1(df))
+# 
+# fix_likert_1 <- function (df){
+#   for (i in seq_along(df)){
+#     if (i == '7 = Extremely satisfied'){
+#       df[[i]] <- 7
+#     }
+#     else if (i == '4 = Neither satisfied nor dissatisfied'){
+#       df[[i]] <- 4
+#     }
+#     else if (i == '1 = Not at all satisfied'){
+#       df[[i]] <- 1
+#     }
+#   }
+# }
+# 
+
+for (i in 1:10){
+  str_replace(survey_data$supervisor_relationship[i], "7 = Extremely satisfied", "7")
+}
+
+
+fix_likert_1(survey_data$supervisor_relationship)
+fix_likert_1(survey_data$work_life_balance)
+
+#map(survey_data, function(df) fix_likert_1(df))
 
 view(survey_data)
 
