8af8692ff08ff01b48ff39a8f6c2f946208e0fd2
Gerlofs <jgerlofs@ead.ubc.ca>
2020-03-08 20:26:57 -0700

    continued working on EDA

---
+++ scripts/EDA.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
+#Load in survey_data csv file
+survey_data <- read.csv(here::here("data", "survey_data.csv"))
+view(survey_data)
+
+fix_likert_1 <- function (df){
+  for (i in seq_along(df)){
+    if (i == '7 = Extremely satisfied'){
+      i = 7
+    }
+    else if (i == '4 = Neither satisfied nor dissatisfied'){
+      i = 4
+    }
+    else if (i == '1 = Not at all satisfied'){
+      i = 1
+    }
+  }
+  
+}
+
+map(survey_data, fix_likert_1(df))
+
+view(survey_data)
+
+ggplot(survey_data, aes(current_living_location)) +
+  geom_bar(width = .8, fill = "steelblue") +
+  coord_flip() +
+  xlab("Continent") +
+  ylab("Number of Graduate Students") +
+  theme_minimal()
